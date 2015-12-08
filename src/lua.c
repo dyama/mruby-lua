c193ca73d9dd6f45c1de2b339a832f068f6aed0b
dyama <dyama@member.fsf.org>
2015-12-08 11:33:46 +0900

    Add dofile and [] method.

--- src/lua.c
+++ src/lua.c
@@ -10,17 +10,16 @@
 
 void luaL_openlibs(lua_State*);
 
-void class_lua_final(mrb_state* mrb, void* p)
+void lua_final(mrb_state* mrb, void* p)
 {
   lua_State* L = p;
   lua_close(L);
   // mrb_free(mrb, p);
 }
 
-static struct mrb_data_type class_lua_type
-  = { "Lua", class_lua_final };
+static struct mrb_data_type class_lua_type = { "Lua", lua_final };
 
-mrb_value class_lua_init(mrb_state* mrb, mrb_value self)
+mrb_value lua_init(mrb_state* mrb, mrb_value self)
 {
   lua_State* L = luaL_newstate();
   luaL_openlibs(L);
@@ -29,23 +28,93 @@ mrb_value class_lua_init(mrb_state* mrb, mrb_value self)
   return self;
 }
 
-mrb_value class_lua_dostring(mrb_state* mrb, mrb_value self)
+mrb_value lua_to_mrb(mrb_state* mrb, lua_State* L, int index)
+{
+  mrb_value result;
+  switch (lua_type(L, index)) {
+    case LUA_TNIL:
+      result = mrb_nil_value();
+      break;
+    case LUA_TNUMBER:
+      ;
+      lua_Integer n = lua_tointeger(L, index);
+      lua_Number  f = lua_tonumber(L, index);
+      if (n != f) {
+        result = mrb_float_value(mrb, f);
+      }
+      else {
+        result = mrb_fixnum_value(n);
+      }
+      break;
+    case LUA_TBOOLEAN:
+      result = lua_toboolean(L, index) ? mrb_true_value() : mrb_false_value(); 
+      break;
+    case LUA_TSTRING:
+      result = mrb_str_new_cstr(mrb, lua_tostring(L, index));
+      break;
+    case LUA_TFUNCTION:
+      if (lua_iscfunction(L, index)) {
+        lua_CFunction cf = lua_tocfunction(L, index);
+        result = mrb_cptr_value(mrb, cf);
+      }
+      else if (lua_isfunction(L, index)) {
+        // Not impremented.
+        result = mrb_nil_value();
+      }
+      break;
+    case LUA_TTABLE:
+      result = mrb_str_new_cstr(mrb, "DEBUG");
+      break;
+    case LUA_TUSERDATA:
+    case LUA_TLIGHTUSERDATA:
+    case LUA_TTHREAD:
+    default:
+        // Not impremented.
+      result = mrb_nil_value();
+      break;
+  }
+  return result;
+}
+
+mrb_value lua_dostring(mrb_state* mrb, mrb_value self)
 {
   mrb_value str;
-  int argc = mrb_get_args(mrb, "S", &str);
+  mrb_get_args(mrb, "S", &str);
   lua_State* L = DATA_PTR(self);
   if (luaL_dostring(L, RSTRING_PTR(str))) {
-    mrb_raise(mrb, E_ARGUMENT_ERROR, "Failed to execute Lua.");
+    mrb_raise(mrb, E_SCRIPT_ERROR, lua_tostring(L, -1));
   }
-  return mrb_nil_value();
+  return lua_to_mrb(mrb, L, -1);
+}
+
+mrb_value lua_dofile(mrb_state* mrb, mrb_value self)
+{
+  mrb_value path;
+  mrb_get_args(mrb, "S", &path);
+  lua_State* L = DATA_PTR(self);
+  if (luaL_dofile(L, RSTRING_PTR(path))) {
+    mrb_raise(mrb, E_SCRIPT_ERROR, lua_tostring(L, -1));
+  }
+  return lua_to_mrb(mrb, L, -1);
+}
+
+mrb_value lua_global(mrb_state* mrb, mrb_value self)
+{
+  mrb_value key;
+  mrb_get_args(mrb, "S", &key);
+  lua_State* L = DATA_PTR(self);
+  lua_getglobal(L, RSTRING_PTR(key));
+  return lua_to_mrb(mrb, L, -1);
 }
 
 void mrb_mruby_lua_gem_init(mrb_state* mrb)
 {
   struct RClass* rclass = mrb_define_class(mrb, "Lua", mrb->object_class);
   MRB_SET_INSTANCE_TT(rclass, MRB_TT_DATA);
-  mrb_define_method(mrb, rclass, "initialize", class_lua_init, MRB_ARGS_NONE());
-  mrb_define_method(mrb, rclass, "dostring",  class_lua_dostring,MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, rclass, "initialize", lua_init,     MRB_ARGS_NONE());
+  mrb_define_method(mrb, rclass, "dostring",   lua_dostring, MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, rclass, "dofile",     lua_dofile,   MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, rclass, "[]",         lua_global,   MRB_ARGS_REQ(1));
   return;
 }
 
